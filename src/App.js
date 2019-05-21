448f57324bc92762b855e74a10772bcfd060182e
Ilia Makhonin <noizemkutane77@gmail.com>
2019-05-21 22:46:30 +0300

    Connect with redux by react-redux, and hot-loader enable

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader/root';
+import TodoContainer from 'containers/TodoContainer';
+import { Provider } from 'react-redux';
+import store from 'store';
+
+
+class App extends Component {
+    render() {
+        return (
+            <Provider store={ store }>
+                <div className="wrapper">
+                    <TodoContainer />
+                </div>
+            </Provider>
+        );
+    }
+}
+
+
+export default hot(App);
