9c0ba316a0323bfd7f16d7e983b8e6fa73360b0f
Ilia Makhonin <noizemkutane77@gmail.com>
2019-05-22 23:46:50 +0300

    Defined propTypes for TaskInput Component and added classes names to the Component

--- src/app/components/TaskInput.js
+++ src/app/components/TaskInput.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 
 class TaskInput extends Component {
@@ -14,15 +15,42 @@ class TaskInput extends Component {
 
     render() {
         return (
-            <div>
-                <input ref={ node => this.inputTask = node } type="text"/>
-                <textarea ref={ node => this.textDescription = node } />
-                <input ref={ node => this.taskDate = node } type="date"/>
-                <button onClick={() => { this.props.addTask(this._getData()) }}>Add</button>
+            <div className="task-input">
+                <label className="task-input_label">
+                    <input
+                        className="task-input_input"
+                        ref={ node => this.inputTask = node }
+                        type="text"
+                    />
+                </label>
+                <label className="task-input_label">
+                    <textarea
+                        className="task-input_textarea"
+                        ref={ node => this.textDescription = node }
+                    />
+                </label>
+                <label className="task-input_label">
+                    <input
+                        className="task-input_input"
+                        ref={ node => this.taskDate = node }
+                        type="date"
+                    />
+                </label>
+                <button
+                    className="task-input_btn-add"
+                    onClick={() => { this.props.addTask(this._getData()) }}
+                >
+                    Add
+                </button>
             </div>
         );
     }
 }
+
+
+TaskInput.propTypes = {
+    addTask: PropTypes.func.isRequired
+};
 
 
 export default TaskInput;
